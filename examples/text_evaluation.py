8e6f1d6ba55198f7323c6af0bb09dcc18a685938
awe-f0x <small-horse@yandex.ru>
2025-07-15 00:52:41 +0300

    Update text_evaluation.py

--- examples/text_evaluation.py
+++ examples/text_evaluation.py
@@ -2,12 +2,13 @@ import requests
 
 def words_count(text):
     words = text.split()
-    return (len(words) - 2)
+    return (len(words))
 
-def get_score(model_id, text):
+def get_score(model_id, text, auth_token):
     payload = {'model_id':model_id, 'text':text}
-    
-    resp = requests.post('http://skolkovo.cbrai.ru/api/v1/score', json=payload)
+    headers = {'Authorization': auth_token}
+
+    resp = requests.post('http://skolkovo.cbrai.ru/api/v1/score', json=payload, headers=headers)
 
     if resp.status_code == 200:
         return resp.json()['score']
@@ -15,10 +16,12 @@ def get_score(model_id, text):
         print(f"Error acquired: {resp.status_code}, {resp.text}")
         return 'Error'
 
-text = 'Some text here'
+text = 'some text here'
+token = 'here will be the auth token that you will receive from the project curators'
+
 wc = words_count(text)
-ARIscore = get_score('ARIEstimator', text)
-Alinascore = get_score('AlinaEstimator', text)
+ARIscore = get_score('ARIEstimator', text, token)
+Alinascore = get_score('AlinaEstimator', text, token)
 
 # save to json file if necessary
 from pathlib import Path
@@ -34,12 +37,11 @@ except:
     data = {}
 
 rnd_uid = str(uuid.uuid4())
-res_data = {
-    "guid" : rnd_uid,
-    'ARIEstimator' : ARIscore,
-    'AlinaEstimator' : Alinascore,
-    'words_count': wc
-}
+res_data = {rnd_uid : {'guid':rnd_uid, 'ARIEstimator' : ARIscore, 'AlinaEstimator' : Alinascore,'words_count': wc}}
+if len(data) == 0:
+    ndata = res_data
+else:
+    ndata = data | res_data
 
-with open(results_file, 'a', encoding='utf-8') as jsf:
-    data = json.dump(data, jsf)
+with open(results_file, 'w', encoding='utf-8') as jsf:
+    json.dump(ndata, jsf)
